806f20e32445cb84331c23740c3ac06190c17a64
Yao <xyy_1214@163.com>
2019-04-21 02:44:13 +0800

    initRouters
    Signed-off-by: Yao <xyy_1214@163.com>

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -6,21 +6,34 @@
  */
 
 class Bootstrap extends Yaf\Bootstrap_Abstract {
-    public function _initConfig()
-    {
-        $config = \Yaf\Application::app()->getConfig();
-        \Yaf\Registry::set('config', $config);
+    private $_config;
+
+    public function _initConfig() {
+        $this->_config = Yaf\Application::app()->getConfig();
+        \Yaf\Registry::set('config', $this->_config);
+    }
+
+    public function _initRouters(Yaf\Dispatcher $dispatcher) {
+        $router = $dispatcher->getRouter();
+        $route  = new Yaf\Route\Rewrite(
+            "/login",
+            array(
+                "controller" => "index",
+                "action"     => "login",
+            )
+        );
+
+        $router->addRoute('login', $route);
     }
 
     public function _initDB() {
-        $config =  Yaf\Registry::get('config');
         $options = [
-            'database_type' => $config->application->db->database_type,
-            'database_name' => $config->application->db->database_name,
-            'server' => $config->application->db->server,
-            'username' => $config->application->db->username,
-            'password' => $config->application->db->password,
-            'charset' => $config->application->db->charset,
+            'database_type' => $this->_config->application->db->database_type,
+            'database_name' => $this->_config->application->db->database_name,
+            'server' => $this->_config->application->db->server,
+            'username' => $this->_config->application->db->username,
+            'password' => $this->_config->application->db->password,
+            'charset' => $this->_config->application->db->charset,
         ];
         $db = new \Medoo\Medoo($options);
         Yaf\Registry::set('db', $db);
--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,9 +1,11 @@
 <?php
 
 class IndexController extends Yaf\Controller_Abstract {
-    public function indexAction() {//默认Action
-        $db = \Yaf\Registry::get('db');
-        $res = $db->select('t_admin','*');
-        $this->getView()->assign("content", $res);
+    public function IndexAction() {
+        echo 'index_index';
+    }
+
+    public function LoginAction() {
+        echo 'index_login';
     }
 }
